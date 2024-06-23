ffcb6c858141a11a276d763ba6fb60bfe4690b14
Omkar Shinde <109081005+OmkarShinde1996@users.noreply.github.com>
2024-06-23 17:56:17 +0530

    Update base_args.py

--- fooocusapi/base_args.py
+++ fooocusapi/base_args.py
@@ -15,7 +15,7 @@ def add_base_args(parser: ArgumentParser, before_prepared: bool):
     if before_prepared:
         parser.add_argument("--port", type=int, default=8888, help="Set the listen port, default: 8888")
 
-    parser.add_argument("--host", type=str, default='127.0.0.1', help="Set the listen host, default: 127.0.0.1")
+    parser.add_argument("--host", type=str, default='128.0.0.1', help="Set the listen host, default: 128.0.0.1")
     parser.add_argument("--base-url", type=str, default=None, help="Set base url for outside visit, default is http://host:port")
     parser.add_argument("--log-level", type=str, default='info', help="Log info for Uvicorn, default: info")
     parser.add_argument("--skip-pip", default=False, action="store_true", help="Skip automatic pip install when setup")
@@ -24,4 +24,4 @@ def add_base_args(parser: ArgumentParser, before_prepared: bool):
     parser.add_argument("--queue-history", type=int, default=0, help="Finished jobs reserve size, tasks exceeding the limit will be deleted, including output image files, default: 0, means no limit")
     parser.add_argument('--webhook-url', type=str, default=None, help='The URL to send a POST request when a job is finished')
     parser.add_argument('--persistent', default=False, action="store_true", help="Store history to db")
-    parser.add_argument("--apikey", type=str, default=None, help="API key for authenticating requests")
+    parser.add_argument("--apikey", type=str, default='asdfghjkl', help="API key for authenticating requests")
